f1066b4362690e37dd503c355bc0f30d5be9570c
Jay Cormier <jay.cormier@me.com>
2023-02-01 17:49:31 +0100

    modified home, added a Getting started section

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -61,7 +61,7 @@ const config = {
         items: [
           {
             type: 'doc',
-            docId: 'intro',
+            docId: 'home',
             position: 'left',
             label: 'Help',
           },
