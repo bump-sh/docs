791ad99de6a8d99f66bf4f3f3be51dba6c0b9078
Christophe Dujarric <christophe@bump.sh>
2023-08-08 13:23:24 +0200

    commas

--- sidebars.js
+++ sidebars.js
@@ -23,9 +23,9 @@ const sidebars = {
         collapsible: true,
         collapsed: true,
         items: [
-          'getting-started/quick-start'
-          'getting-started/api-platform'
-          'getting-started/fastapi'
+          'getting-started/quick-start',
+          'getting-started/api-platform',
+          'getting-started/fastapi',
         ],
       },
       'bump-cli',
