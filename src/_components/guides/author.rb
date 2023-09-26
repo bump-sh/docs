29ee784c4c59d075bd3a0a20a7d7b1cc6777a12b
Thimy Kieu <thimy@bump.sh>
2023-09-26 12:28:25 +0200

    improve author card

--- src/_components/guides/author.rb
+++ src/_components/guides/author.rb
@@ -2,4 +2,8 @@ class Guides::Author < Bridgetown::Component
   def initialize(author:)
     @author = Bridgetown::Current.site.data.authors[author]
   end
+
+  def render?
+    @author.present?
+  end
 end
