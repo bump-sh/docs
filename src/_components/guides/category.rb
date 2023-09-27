734e38798f4922cf039e78d52a9959ef0a99d315
Thimy Kieu <thimy@bump.sh>
2023-09-27 17:14:28 +0200

    remove all guide content

--- src/_components/guides/category.rb
+++
@@ -1,8 +0,0 @@
-class Guides::Category < Bridgetown::Component
-  def initialize(resources:, title:, css_classes: "", position: nil)
-    @resources = resources
-    @title = title
-    @css_classes = css_classes
-    @position = position
-  end
-end
