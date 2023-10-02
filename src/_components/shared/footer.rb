a5c820b229a263f98d11114aae7cbb4025f32a9d
Thimy Kieu <thimy@bump.sh>
2023-10-02 16:25:21 +0200

    help: separate scroll for sidebar and content and remove footer

--- src/_components/shared/footer.rb
+++ src/_components/shared/footer.rb
@@ -1,5 +1,10 @@
 class Shared::Footer < Bridgetown::Component
-  def initialize
+  def initialize(show:)
+    @show = show
     @site = Bridgetown::Current.site
+  end
+
+  def render?
+    @show
   end
 end
