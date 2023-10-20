1d3565fe3a9b67a8eabc1182d801349eb1a9c2a3
Thimy Kieu <thimy@bump.sh>
2023-10-20 11:04:56 +0200

    slugify dot in category

---
+++ src/_components/categories/sidebar/item.rb
@@ -0,0 +1,14 @@
+class Categories::Sidebar::Item < Bridgetown::Component
+  def initialize(category:, current:)
+    @category = category
+    @current = current
+  end
+
+  def slugified_name
+    Bridgetown::Utils.slugify(@category.name)
+  end
+
+  def is_current?
+    slugified_name == Bridgetown::Utils.slugify(@current)
+  end
+end
