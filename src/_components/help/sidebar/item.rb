6eaf2d8eb99693864e75ccac9bcfb5ea6d875d24
Thimy Kieu <thimy@bump.sh>
2023-11-03 12:10:23 +0100

    docs: make categories collapsible

--- src/_components/help/sidebar/item.rb
+++ src/_components/help/sidebar/item.rb
@@ -3,9 +3,23 @@ class Help::Sidebar::Item < Bridgetown::Component
     @page = page
     @current = current
     @resource = @page[:resource]
-    @is_current = @resource.present? && @resource.path == @current.path
     @icon = @page[:icon]
     @label = @page[:label]
     @items = @page[:items]
+    @is_current = @resource&.path == @current.path
+    @is_parent = is_parent
+  end
+
+  def category_id
+    @label.parameterize
+  end
+
+  def is_parent
+    if @items.present?
+      current_child = @items&.find do |item|
+        item[:resource].path == @current.path
+      end
+      return !!current_child
+    end
   end
 end
