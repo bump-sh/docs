fc8c9d742e3080b2f9018be0d1fb65fd2f22f739
Sébastien Charrier <scharrier@gmail.com>
2026-03-20 16:08:44 +0100

    Fix sidebar item ID duplication
    Prefix nested sidebar item IDs with parent category ID to avoid
    collisions when multiple categories share child labels.

--- src/_components/shared/sidebar/item.rb
+++ src/_components/shared/sidebar/item.rb
@@ -1,7 +1,8 @@
 class Shared::Sidebar::Item < Bridgetown::Component
-  def initialize(page:, current:)
+  def initialize(page:, current:, parent_id: nil)
     @page = page
     @current = current
+    @parent_id = parent_id
     @resource = @page[:resource]
     @icon = @page[:icon]
     @label = @page[:label]
@@ -11,7 +12,11 @@ class Shared::Sidebar::Item < Bridgetown::Component
   end
 
   def category_id
-    @label.parameterize
+    if @parent_id
+      "#{@parent_id}--#{@label.parameterize}"
+    else
+      @label.parameterize
+    end
   end
 
   def is_parent
